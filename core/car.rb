6ad0abe09efca4b9b145b4aa58bc00879d9243ff
Nathaniel Jones <nj@third.io>
2013-12-04 11:40:26 -0600

    Car, Expenses

---
+++ core/car.rb
@@ -0,0 +1,30 @@
+
+
+
+          class Car
+            class << self
+              def advertise(model)
+                puts "BUY IT NOW! #{model}"
+              end
+
+              def manufacture(make)
+                Car.new(make)
+              end
+            end
+
+            def initialize(make)
+              @make = make
+            end
+
+            def drive(miles)
+              puts "Driving #{miles} miles"
+            end
+          end
+
+          civic = Car.new('Civic')
+          civic.drive(30)
+
+          Car.manufacture('Ford')
+          Car.advertise('Tesla Model S')
+
+
